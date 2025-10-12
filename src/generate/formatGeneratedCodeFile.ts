2650b1fe599c04c4b557d07201275eb93a6f6ea4
David Siewert <david1gruppenplan@gmail.com>
2025-10-12 14:39:47 +0600

    chore(generate): add code generation for barrel imports

---
+++ src/generate/formatGeneratedCodeFile.ts
@@ -0,0 +1,6 @@
+import { runCmdAsync } from "@utils/bun/runCmdAsync"
+
+export async function formatGeneratedCodeFile(outputPath: string) {
+  const cmd = `bun run biome check --write ${outputPath}`.split(" ")
+  return runCmdAsync(cmd, true)
+}
