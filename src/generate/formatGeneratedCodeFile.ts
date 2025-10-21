494931fdf8c94c531fd5b3b0b0cdad6a6cda3668
David Siewert <david1gruppenplan@gmail.com>
2025-10-21 20:19:24 +0600

    chore(index): remove barrel code generation

--- src/generate/formatGeneratedCodeFile.ts
+++
@@ -1,6 +0,0 @@
-import { runCmdAsync } from "~utils/bun/runCmdAsync"
-
-export async function formatGeneratedCodeFile(outputPath: string) {
-  const cmd = `bun run biome check --write ${outputPath}`.split(" ")
-  return runCmdAsync(cmd, true)
-}
