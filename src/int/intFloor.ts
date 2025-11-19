978602130f05c8daf24b2ac8ba4164f8e10646aa
David Siewert <david1gruppenplan@gmail.com>
2025-11-19 08:25:34 +0600

    feat(int): add floor

---
+++ src/int/intFloor.ts
@@ -0,0 +1,11 @@
+export function intFloor(n: number) {
+  return Math.floor(n)
+}
+
+export function intFloor5(n: number) {
+  return Math.floor(n / 5) * 5
+}
+
+export function intFloor10(n: number) {
+  return Math.floor(n / 10) * 10
+}
