43ff0f337c04788d134fd8cf02c0b910b64c0d64
David Siewert <david1gruppenplan@gmail.com>
2025-12-04 11:58:23 +0600

    refactor(num/format): to follow subject-first naming pattern

--- src/num/numFormat.ts
+++ src/num/numFormat.ts
@@ -1,7 +1,7 @@
 let numberFormat: Intl.NumberFormat | undefined = undefined
 let numberFormatLocale: string | undefined = undefined
 
-export function formatNumber(num: number, locale?: string) {
+export function numFormat(num: number, locale?: string) {
   if (locale === numberFormatLocale && numberFormat) {
     return numberFormat.format(num)
   }
