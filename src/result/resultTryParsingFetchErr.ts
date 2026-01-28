aec2721d9bd0588d8af2d1a7c720039c9f52a334
David Siewert <david1gruppenplan@gmail.com>
2026-01-28 08:09:38 +0600

    feat(result): add resultTryParsingFetchErr

---
+++ src/result/resultTryParsingFetchErr.ts
@@ -0,0 +1,16 @@
+import * as a from "valibot"
+import { createError } from "~utils/result/Result"
+import { resultErrSchemaFromJsonString } from "~utils/result/resultErrSchema"
+
+export function resultTryParsingFetchErr(op: string, text: string, statusCode: number, responseStatusText: string) {
+  const op2 = "resultTryParsingFetchErr"
+  const parsingErr = a.safeParse(resultErrSchemaFromJsonString, text)
+  if (!parsingErr.success) {
+    const e = createError(op + "." + op2, responseStatusText, text)
+    e.statusCode = statusCode
+    return e
+  }
+  const e = parsingErr.output
+  e.statusCode = statusCode
+  return e
+}
