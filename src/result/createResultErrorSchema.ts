19e9cab196932ebb29db5bc374d1e4c557a37b87
David Siewert <david1gruppenplan@gmail.com>
2026-03-15 11:19:47 +0600

    refactor(result): remove result, migrated into separate package @adaptive-ds/result, remove dayjs dependency, move typescript to dev deps

--- src/result/createResultErrorSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as a from "valibot"
-import { type ResultErr, createResultError } from "./Result"
-import { stringifyValibotErrors } from "../valibot/stringifyValibotErrors"
-
-export function createResultErrorSchema(op: string, valibotError: a.ValiError<any>, errorData?: string): ResultErr {
-  // const errorMessage = valibotError.issues.map((issue) => issue.message).join(", ")
-  const errorMessage = stringifyValibotErrors(valibotError)
-  return createResultError(op, errorMessage, errorData)
-}
